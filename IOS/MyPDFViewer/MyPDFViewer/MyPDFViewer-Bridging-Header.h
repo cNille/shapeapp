9b9464201bd00ac38f9f7f1e92079c9e86682537
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2015-04-28 21:46:13 +0200

    Bridged between objective c and swift

---
+++ IOS/MyPDFViewer/MyPDFViewer/MyPDFViewer-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  -Bridging-Header.h
+//  MyPDFViewer
+//
+//  Created by Christopher Ni on 28/04/15.
+//  Copyright (c) 2015 ShapeApp. All rights reserved.
+//
+
+#ifndef MyPDFViewer_MyPDFViewer_Bridging_Header_h
+#define MyPDFViewer_MyPDFViewer_Bridging_Header_h
+
+#import "/Users/nille/Dropbox/Nille/Kod/IOS-Libraries/Reader-master/Sources/ReaderViewController.h"
+
+#endif
--- IOS/MyPDFViewer/MyPDFViewer/ViewController.swift
+++ IOS/MyPDFViewer/MyPDFViewer/ViewController.swift
@@ -8,10 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+class ViewController: ReaderViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +26,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func didClickOpenPDF(sender: UIButton){
+        let file: NSString = NSBundle.mainBundle().pathForResource("typo_tips", ofType: "pdf")!
+        
+    }
 
 }
 
