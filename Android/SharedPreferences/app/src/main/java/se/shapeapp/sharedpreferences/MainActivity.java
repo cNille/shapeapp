c12667f377ea6240ce0a77890705d676fc3e2799
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2015-06-22 16:35:14 +0200

    Implemented the sharedpref sample

--- Android/SharedPreferences/app/src/main/java/se/shapeapp/sharedpreferences/MainActivity.java
+++ Android/SharedPreferences/app/src/main/java/se/shapeapp/sharedpreferences/MainActivity.java
@@ -1,37 +1,53 @@
 package se.shapeapp.sharedpreferences;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String MY_PREFS_NAME = "MyPreferencesFileName";
+    private TextView current_text;
+    private EditText my_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Load our textview and edittext.
+        current_text = (TextView) findViewById(R.id.current_text);
+        my_text = (EditText) findViewById(R.id.my_text);
+
+        //Get the sharedpreferences for our file.
+        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+
+        //Get the value of my_text.
+        String text = prefs.getString("my_text", "Hello world!");
+
+        //Update the current_text with the new text.
+        current_text.setText(text);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void saveClick(View v){
+        //Get the text from our edittext.
+        String text = my_text.getText().toString();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        //Load the editor for our sharedpref file.
+        SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //Update my_text with the new text.
+        editor.putString("my_text", text);
 
-        return super.onOptionsItemSelected(item);
+        //Commit the changes.
+        editor.commit();
+
+        //Update the current_text with the new text.
+        current_text.setText(text);
     }
 }
