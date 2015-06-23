938a9df2884dd2d84fe87df51f896fba4de61257
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2015-06-23 22:29:07 +0200

    Fixed scroll in mobile-mode

--- Wordpress/Plugins/shapeapp-parallax-image-widget/myjs.js
+++ Wordpress/Plugins/shapeapp-parallax-image-widget/myjs.js
@@ -1,6 +1,7 @@
-
-	skrollr.init({
+if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
+    skrollr.init({
+        forceHeight: false,
 		smoothScrolling: true,
 		mobileDeceleration: 0.004
-	});
-	
+    });
+}
