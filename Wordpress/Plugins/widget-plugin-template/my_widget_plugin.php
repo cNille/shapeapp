23c1ddfe397ba5ddc073122d73b87d09be8241e5
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2015-06-25 22:45:44 +0200

    Added a tutuorial code on implementing admin page for a plugin.

--- Wordpress/Plugins/widget-plugin-template/my_widget_plugin.php
+++ Wordpress/Plugins/widget-plugin-template/my_widget_plugin.php
@@ -15,6 +15,7 @@
     // 1.0 Enqueue scripts
     // 2.0 Create Widget
     // 3.0 Register Widget
+    // 4.0 Creating the plugins administrators page.
 
 
 
@@ -120,4 +121,65 @@
 		register_widget( 'img_parallax_widget' );
 	}
 	add_action( 'widgets_init', 'img_parallax_load_widget' );
+
+
+
+    //============================================================
+    // 4.0 Creating the plugins administrators page.
+    //============================================================
+
+	//TODO: Simplify code!!
+
+    function widget_plugin_admin(){
+    	include('widget_plugin_admin.php');
+    }
+
+	function my_plugins_admin_actions() {
+		//Adds the sub-menu item to the settings item in the admin menu.
+ 		add_options_page("My Widget plugin settings", "My Widget plugin settings", 1, "My Widget plugin settings", "my_plugin_admin");
+
+		//Would add the sub-menu item to the tools item in the admin menu.
+ 		//add_management_page("My Widget plugin settings", "My Widget plugin settings", 1, "My Widget plugin settings", "my_plugin_admin");
+	}	 
+	add_action('admin_menu', 'my_plugins_admin_actions');
+
+	function oscimp_getproducts($product_cnt=1) {
+	    //Connect to the OSCommerce database
+		$oscommercedb = new wpdb(get_option('oscimp_dbuser'),get_option('oscimp_dbpwd'), get_option('oscimp_dbname'), get_option('oscimp_dbhost'));
+	
+		$retval = '';
+	    for ($i=0; $i<$product_cnt; $i++) {
+	        //Get a random product
+	        $product_count = 0;
+	        while ($product_count == 0) {
+	            $product_id = rand(0,30);
+	            $product_count = $oscommercedb->get_var("SELECT COUNT(*) FROM products WHERE products_id=$product_id AND products_status=1");
+	        }
+	         
+	        //Get product image, name and URL
+	        $product_image = $oscommercedb->get_var("SELECT products_image FROM products WHERE products_id=$product_id");
+	        $product_name = $oscommercedb->get_var("SELECT products_name FROM products_description WHERE products_id=$product_id");
+	        $store_url = get_option('oscimp_store_url');
+	        $image_folder = get_option('oscimp_prod_img_folder');
+	 
+	        //Build the HTML code
+	        $retval .= '<div class="oscimp_product">';
+	        $retval .= '<a href="'. $store_url . 'product_info.php?products_id=' . $product_id . '"><img src="' . $image_folder . $product_image . '" /></a><br />';
+	        $retval .= '<a href="'. $store_url . 'product_info.php?products_id=' . $product_id . '">' . $product_name . '</a>';
+	        $retval .= '</div>';
+	 
+	    }
+	    return $retval;
+	}
+
+
+	//Insert this to template to use plugin:
+	/*
+	 *	<li><?php echo oscimp_getproducts(3); ?></li> 
+	*/
+
+
+
+
+
 ?>
